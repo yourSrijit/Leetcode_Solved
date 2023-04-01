a31090d3cb90e9ab718fe8f0fc8256e273296006
Srijit Bera <berasrijit02@gmail.com>
2023-04-01 20:24:48 +0530

    Create 1. Two Sum.java

---
+++ 1. Two Sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        int[] arr = new int[2];
+        
+        for(int i = 0; i < nums.length; i++) {
+            hm.put(nums[i], i);
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(hm.containsKey(complement) && hm.get(complement) != i) {
+                arr[0] = i;
+                arr[1] = hm.get(complement);
+                break;
+            }
+        }
+        
+        return arr;
+    }
+}
