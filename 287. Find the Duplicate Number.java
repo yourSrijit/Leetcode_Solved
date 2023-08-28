84330d1b865f09ba65dd5d7f1f75f9870f17c243
Srijit Bera <berasrijit02@gmail.com>
2023-08-28 22:54:59 +0530

    Create 287. Find the Duplicate Number.java

---
+++ 287. Find the Duplicate Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            if(e.getValue()>1){
+                return e.getKey();
+            }
+        }
+        return 1;
+    }
+}
