0e5c8c0b8fa12162ca0cee39c5c8847068094b2e
Srijit Bera <berasrijit02@gmail.com>
2023-09-18 19:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (59.37%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,26 +1,47 @@
+// class Solution {
+//     public int findPeakElement(int[] nums) {
+//         if(nums.length==1) return 0;
+//         int end=nums.length-1;
+//         int start=0;
+//         int max=nums[start];
+//         int index=0;
+//         while(start<end){
+//             if(nums[start]<=nums[end]){
+//                 if(nums[end]>=max){
+//                     max=nums[end];
+//                     index=end;
+//                 }
+//                 start++;
+//             }
+//             else{
+//                 if(nums[start]>=max){
+//                     max=nums[start];
+//                     index=start;
+//                 }
+//                 end--;
+//             }
+//         }
+//         return index;
+//     }
+// }
+
+
 class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length==1) return 0;
-        int end=nums.length-1;
-        int start=0;
-        int max=nums[start];
-        int index=0;
-        while(start<end){
-            if(nums[start]<=nums[end]){
-                if(nums[end]>=max){
-                    max=nums[end];
-                    index=end;
-                }
-                start++;
-            }
-            else{
-                if(nums[start]>=max){
-                    max=nums[start];
-                    index=start;
-                }
-                end--;
+        // if length is 1
+        if(nums.length== 1)
+            return 0;
+        int max= Integer.MIN_VALUE;
+        int index= Integer.MIN_VALUE;
+
+        // Finding MAX element and INDEX
+        for(int i= 0; i< nums.length; i++){
+            if(max< nums[i]){
+                max= nums[i];
+                index= i;
             }
         }
+
         return index;
     }
 }
