c5e6eb2233106f42e6bd547b22eade25597cf357
Srijit Bera <berasrijit02@gmail.com>
2023-10-08 20:39:45 +0530

    Time: 5 ms (68.12%) | Memory: 43.6 MB (57.09%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,8 +1,9 @@
+
 class Solution {
   public int minDistance(String word1, String word2) {
-    final int m = word1.length();//first word length
-    final int n = word2.length();///second word length
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
+    final int m = word1.length();
+    final int n = word2.length();
+
     int[][] dp = new int[m + 1][n + 1];
 
     for (int i = 1; i <= m; ++i)
@@ -13,10 +14,10 @@ class Solution {
 
     for (int i = 1; i <= m; ++i)
       for (int j = 1; j <= n; ++j)
-        if (word1.charAt(i - 1) == word2.charAt(j - 1))//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
+        if (word1.charAt(i - 1) == word2.charAt(j - 1))
+          dp[i][j] = dp[i - 1][j - 1];
         else
-          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                      //replace               //delete        //insert
+          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                     
 
     return dp[m][n];
   }
