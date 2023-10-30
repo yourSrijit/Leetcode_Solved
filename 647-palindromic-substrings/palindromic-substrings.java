eae17e90236d0dbe63efc5993a9388d81787faf1
Srijit Bera <berasrijit02@gmail.com>
2023-10-30 21:45:10 +0530

    Time: 13 ms (28.82%) | Memory: 43.9 MB (19.62%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,26 @@
+class Solution {
+   public int countSubstrings(String s) {
+        int l=s.length();
+        int count=0;
+        boolean dp[][]=new boolean[l][l];
+        for(int g=0;g<l;g++){
+            for(int i=0, j=g;j<l;j++,i++){
+                if(g==0){
+                    dp[i][j]=true;
+                }
+                else if(g==1){
+                    dp[i][j]=s.charAt(i)==s.charAt(j);
+                }
+                else {
+                    if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true){
+                        dp[i][j]=true;
+                    }
+                }
+                if(dp[i][j]==true){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
