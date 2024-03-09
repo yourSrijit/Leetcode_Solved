e25060f30690c14b7efef8950c783f98bfcaaae4
Srijit Bera <berasrijit02@gmail.com>
2024-03-09 08:37:16 +0530

    Time: 1 ms (76.73%) | Memory: 41.2 MB (65.72%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String words[]=s.split(" ");
-        if(words.length!=pattern.length()){
+        HashMap<Character,String> ch=new HashMap<>();
+         HashMap<String,Character> wd=new HashMap<>();
+        String words[]=s.split(" +");
+        if(pattern.length()!=words.length){
             return false;
         }
-        int l=pattern.length();
-        Map<Character,String>ch =new HashMap<>();
-        Map<String,Character>wd =new HashMap<>();
-        for(int i=0;i<l;i++){
-                char c=pattern.charAt(i);
-                String word=words[i];
-                if(!ch.containsKey(c)){
-                    ch.put(c,word);
-                }
-                if(!wd.containsKey(word)){
-                    wd.put(word,c);
-                }
-                if(!ch.get(c).equals(word) || !wd.get(word).equals(c)){
-                    return false;
-                }
+        for(int i=0;i<pattern.length();i++){
+            char c=pattern.charAt(i);
+            String word=words[i];
+            if(!ch.containsKey(c)){
+                ch.put(c,word);
+            }
+             if(!wd.containsKey(word)){
+                wd.put(word,c);
+            }
+            if(!ch.get(c).equals(word) || !wd.get(word).equals(c)){
+                return false;
+            }
+
         }
         return true;
+        
     }
 }
