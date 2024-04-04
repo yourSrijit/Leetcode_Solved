7ae4dba59ccde425d6364e240a494c326dc9c518
Srijit Bera <berasrijit02@gmail.com>
2024-04-04 23:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (14.21%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,37 @@
-class Solution {
-int[] memo;
-public int rob(int[] nums) {
-    memo = new int[nums.length + 1];
-    Arrays.fill(memo, -1);
-    return rob(nums, nums.length - 1);
-}
+// class Solution {
+//     public int rob(int[] nums) {
+//         if(nums.length<2){
+//             return nums[0];
+//         }
+//         //dp array initilization
+//         int dp[]=new int[nums.length];
+        
+//         //dp value initilize
+//         dp[0]=nums[0];
+//         dp[1]=Math.max(nums[0],nums[1]);
 
-private int rob(int[] nums, int i) {
-    if (i < 0) {
-        return 0;
+//         for(int i=2;i<nums.length;i++){
+//             dp[i]=Math.max(nums[i-2]+nums[i],nums[i-1]);
+//         }
+//         return dp[nums.length-1];
+//     }
+// }
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        
+        return dp[nums.length - 1];
     }
-    if (memo[i] >= 0) {
-        return memo[i];
-    }
-    int result = Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
-    memo[i] = result;
-    return result;
-}
 }
