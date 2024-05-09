1f6551b9daae31874a456df8e7507baca311caa7
Srijit Bera <berasrijit02@gmail.com>
2024-05-09 21:36:23 +0530

    Update two-sum.java

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -9,7 +9,7 @@ class Solution {
             hm.put(nums[i], i);
         }
         
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) { 
             int complement = target - nums[i];
             if(hm.containsKey(complement) && hm.get(complement) != i) {
                 arr[0] = i;
