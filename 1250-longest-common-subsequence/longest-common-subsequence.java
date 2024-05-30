64780770c1c52518778d71a358b8048a2a293fea
Srijit Bera <berasrijit02@gmail.com>
2024-05-30 12:02:48 +0530

    Time: 30 ms (16.88%) | Memory: 50.7 MB (62.19%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -19,7 +19,7 @@ class Solution {
         int n=s2.length();
         int dp[][]=new int[m+1][n+1];
         for(int i=1;i<=m;i++){
-            for(int j=0;j<=n;j++){
+            for(int j=1;j<=n;j++){
                 dp[i][j]=-1;
             }
         }
