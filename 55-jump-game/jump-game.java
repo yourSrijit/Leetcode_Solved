d16c9180e5dc1375ff8517dbe6bebd4cbf083c43
Srijit Bera <berasrijit02@gmail.com>
2024-06-05 20:02:26 +0530

    Time: 1 ms (99.97%) | Memory: 45.4 MB (76.14%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         int finalPosition=nums.length-1;
         int l=nums.length;
-        for(int i=l-2;i>=0;i--){
+        for(int i=l-1;i>=0;i--){ 
             if(i+nums[i]>=finalPosition){
                 finalPosition=i;
             }
