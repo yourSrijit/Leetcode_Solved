4d639605432e44d57dc347cddf9ae8d7bbe95577
Srijit Bera <berasrijit02@gmail.com>
2024-06-29 06:13:26 +0530

    Time: 55 ms (43.05%) | Memory: 46.6 MB (84.07%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -26,7 +26,7 @@ class Solution {
         int m=s1.length();
         int n=s2.length();
         int dp[][]=new int[m+1][n+1];
-        for(int i=1;i<=m;i++){
+        for(int i=0;i<=m;i++){
             for(int j=0;j<=n;j++){
                 dp[i][j]=-1;
             }
